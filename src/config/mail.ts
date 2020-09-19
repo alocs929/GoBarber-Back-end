b080240b053356bfeea6174f5f9f9e0ee20803e9
Bruno Martins <brunorodrigues929@gmail.com>
2020-09-19 05:21:45 -0300

    Envio de email com SES e Ethereal, Arq com S3 e Disk

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -1,7 +1,20 @@
 interface IMailConfig {
   driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
 }
 
 export default {
   driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: '<<email com o meu dominio bruno@titico.com>>',
+      name: 'Titico do GoBarber',
+    },
+  },
 } as IMailConfig;
